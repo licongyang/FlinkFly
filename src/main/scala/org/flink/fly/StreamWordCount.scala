2796ed568cf52b230f228846d12d2b6939915c0b
licongyang <liby-1987@163.com>
2022-11-13 08:37:03 +0800

    [feat]  operator chain   并行度相同，one-to-one算子任务会链接成任务链   - stream在算子之间传输数据的形式可以是one-to-one(farward)的模式，或者redistributing模式，这个取决算子的种类   - one-to-one：后一个算子任务的元素和顺序与前一个算子的元素和顺序相同，比如map\fliter\flatmap等算子 1. env.disableOperatorChaining()   全局限制，每个算子都是一个单独任务 2. operator.startNewChain()   opertator指map、fliter各种算子，，即从当前任务之前断开，重新可以有新开一个任务链

--- src/main/scala/org/flink/fly/StreamWordCount.scala
+++ src/main/scala/org/flink/fly/StreamWordCount.scala
@@ -22,29 +22,33 @@ object StreamWordCount {
     val params = ParameterTool.fromArgs(args)
     val host: String = params.get("host")
     val port: Int = params.getInt("port")
-    // 创建流执行环境
+//     创建流执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+//    全局限制算子合并成任务链
+//    env.disableOperatorChaining()
 
-    // 读取数据流 ： socket文本流 本地起socket
-    // nc -lk 7777
-    // netconnect listen keepalive (保持多个连接，而不会单个连接断开)
+//     读取数据流 ： socket文本流 本地起socket
+//     nc -lk 7777
+//     netconnect listen keepalive (保持多个连接，而不会单个连接断开)
 //    val dataStream = env.socketTextStream("localhost", 7777)
     val dataStream = env.socketTextStream(host, port)
 
-    // 对每条数据进行处理
+//     对每条数据进行处理
     val wordCountDataStream = dataStream.flatMap(_.split(" "))
       .filter(_!= null)
+//      强制从该算子任务开始新的任务链
+      .startNewChain()
       .map((_, 1))
 //       stream 聚合需要用keybu(spark 用 reducebykey)
       .keyBy(0)
       .sum(1)
 
     wordCountDataStream.print()
-      // 可以设置任务并行度，执行线程数量，分到那个线程slot（放开）
-      // .setParallelism(2)
+//       可以设置任务并行度，执行线程数量，分到那个线程slot（放开）
+       .setParallelism(1)
 
-    // wordCountDataStream.writeAsText("/Users/alfie/workspace/code/learn/flink-lean/FlinkFly/data")
-    //启动executor
+//     wordCountDataStream.writeAsText("/Users/alfie/workspace/code/learn/flink-lean/FlinkFly/data")
+//    启动executor
     env.execute("streaming word count")
   }
 
