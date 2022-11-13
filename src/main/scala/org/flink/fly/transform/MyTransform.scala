13bc41f4cd86416ddac567bd91ab5491ae4401bb
licongyang <liby-1987@163.com>
2022-11-13 19:08:51 +0800

    [feat]  custom transform 1.基本转换算子   - map 流元素转换成其他元素   - flatmap 流打散成各个元素的流   - fliter 按照元素特性过滤流元素 2.键控算子   - keyby 流按照key分区 ,返回keyedstream 3.滚动聚合算子   -sum max min maxby minby 基于键控算子，做分区键值聚合 注意：键控算子会重新按照key对流进行分区，并行度 > 1情况下，每条记录重新划分到不同的分区中，处理顺序可能与原始数据顺序不一致，但不影响最终一致性。     保证顺序一致，需要从source\转换算子\sink并行度为1，即按照原始数据顺序一个个单线程处理 4.聚合   -reduce 参数二元组，第一个前一个聚合结果，后一个当前元素 5.多流合并   -split select (新的side out)   -connect comap/map ：合并两个流，两个流数据类型不一样，可以在map/comap中调整数据类型一致   -union 多个流，但各个流数据类型一样

--- src/main/scala/org/flink/fly/transform/MyTransform.scala
+++ src/main/scala/org/flink/fly/transform/MyTransform.scala
@@ -10,6 +10,7 @@ object MyTransform {
 
     val streaFromFile = env.readTextFile("/Users/alfie/workspace/code/learn/flink-lean/FlinkFly/src/main/resources/sensor.txt")
 
+    // 1.基本转换算子和聚合算子
     // map ：返回DataStream ,里面范型可以改变,将之前的String转换为SensorReading
     val dataStream: DataStream[SensorReading] = streaFromFile.map(data => {
       val dataArray = data.split(",")
@@ -25,10 +26,46 @@ object MyTransform {
     // 如果想就一个数据统计来了几个，也是每个元素 前面加一个占位 比如0 ，然后按照该占位聚合
     // 输出当前传感器最新的温度+10，而时间戳是上一次数据的时间+1
     // reduce传入二元参数，x之前聚合结果，y新来的元素
-    val dataSm = keyedStream.reduce((x, y) => SensorReading(x.id, x.timestamp + 1, y.temperature + 10))
+    val aggStream = keyedStream.reduce((x, y) => SensorReading(x.id, x.timestamp + 1, y.temperature + 10))
 
-    dataSm.print()
+    aggStream.print()
     //      .setParallelism(1)
+
+    // 2. 多流合并
+    // split->splitstream 根据某些特性（贴了标识）把一个datastream拆分两个或者多个datastream
+    // 不用直接用聚合操作，需要用select分成多个流
+    // 需求：传感器数据按照温度高低，拆分成两个流
+    val splitStream = dataStream.split( data => {
+      if(data.temperature > 30) Seq("high") else Seq("low")
+    })
+
+    val high = splitStream.select("high")
+    val low = splitStream.select("low")
+    val all = splitStream.select("high","low")
+
+    high.print("high")
+    low.print("low")
+    all.print("all")
+
+    // connect-> connectedStream 同流不合污,整体一个流，但各管各的（两个流数据类型可以不一样，再之后的coMap再去调整一样的）
+    // coMap\coflat , 同时对流中的各个部分应用map，合并一个流（操作也可以不一样）
+    // 只能两个流合并，无法后面再跟connect
+    val warningStream =high.map((data => (data.id, data.temperature)))
+    val connectedStreams = warningStream.connect(low)
+
+    val coMapDataStream = connectedStreams.map(
+      warningData => (warningData._1, warningData._2, "warning"),
+      lowData => (lowData.id, "health")
+    )
+    coMapDataStream.print("coMapDataStream")
+
+    // union 可以对两个或者两个以上的DataStream(流数据类型要求一样)进行合并，形成一个新的DataStream
+    // 顺序可能有变化
+    var unionStream = high.union(low)
+    unionStream.print("union stream")
+
+
+
 
 
     env.execute("my transform")
