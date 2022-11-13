896576f296635f822b34fb7c4c0525478bf2dbc9
licongyang <liby-1987@163.com>
2022-11-13 18:36:40 +0800

    [feat]  custom transform 1.基本转换算子   - map 流元素转换成其他元素   - flatmap 流打散成各个元素的流   - fliter 按照元素特性过滤流元素 2.键控算子   - keyby 流按照key分区 ,返回keyedstream 3.滚动聚合算子   -sum max min maxby minby 基于键控算子，做分区键值聚合 注意：键控算子会重新按照key对流进行分区，并行度 > 1情况下，每条记录重新划分到不同的分区中，处理顺序可能与原始数据顺序不一致，但不影响最终一致性。     保证顺序一致，需要从source\转换算子\sink并行度为1，即按照原始数据顺序一个个单线程处理 4.聚合   -reduce 参数二元组，第一个前一个聚合结果，后一个当前元素

--- src/main/scala/org/flink/fly/transform/MyTransform.scala
+++ src/main/scala/org/flink/fly/transform/MyTransform.scala
@@ -20,7 +20,12 @@ object MyTransform {
     // .sum(1)
     // keyedStream要求范型为T,K；k为Tuple，当用_.id指定为String，范型K可以为String;否则需要按照Tuple定义
     val keyedStream: KeyedStream[SensorReading, String] = dataStream.keyBy(_.id)
-    val dataSm = keyedStream.sum("temperature")
+    //  val dataSm = keyedStream.sum("temperature")
+    // 聚合需要先做分区，借助分布式并行能力
+    // 如果想就一个数据统计来了几个，也是每个元素 前面加一个占位 比如0 ，然后按照该占位聚合
+    // 输出当前传感器最新的温度+10，而时间戳是上一次数据的时间+1
+    // reduce传入二元参数，x之前聚合结果，y新来的元素
+    val dataSm = keyedStream.reduce((x, y) => SensorReading(x.id, x.timestamp + 1, y.temperature + 10))
 
     dataSm.print()
     //      .setParallelism(1)
