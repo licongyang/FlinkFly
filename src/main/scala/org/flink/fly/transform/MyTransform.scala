777bbc067837b57162b42c0ef96a2c167c474753
licongyang <liby-1987@163.com>
2022-11-13 17:57:29 +0800

    [feat]  custom transform 1.基本转换算子   - map 流元素转换成其他元素   - flatmap 流打散成各个元素的流   - fliter 按照元素特性过滤流元素 2.键控算子   - keyby 流按照key分区 ,返回keyedstream 3.滚动聚合算子   -sum max min maxby minby 基于键控算子，做分区键值聚合 注意：键控算子会重新按照key对流进行分区，并行度 > 1情况下，每条记录重新划分到不同的分区中，处理顺序可能与原始数据顺序不一致，但不影响最终一致性。     保证顺序一致，需要从source\转换算子\sink并行度为1，即按照原始数据顺序一个个单线程处理

--- src/main/scala/org/flink/fly/transform/MyTransform.scala
+++ src/main/scala/org/flink/fly/transform/MyTransform.scala
@@ -5,24 +5,25 @@ import org.flink.fly.source.SensorReading
 
 object MyTransform {
 
-  def main(args: Array[String]): Unit= {
+  def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
     val streaFromFile = env.readTextFile("/Users/alfie/workspace/code/learn/flink-lean/FlinkFly/src/main/resources/sensor.txt")
 
     // map ：返回DataStream ,里面范型可以改变,将之前的String转换为SensorReading
-    val dataStream: DataStream[SensorReading] = streaFromFile.map( data =>{
+    val dataStream: DataStream[SensorReading] = streaFromFile.map(data => {
       val dataArray = data.split(",")
       SensorReading(dataArray(0).trim, dataArray(1).trim.toLong, dataArray(2).trim.toDouble)
     })
-      // 聚合，按照温度相加
-      .keyBy(0)
-      .sum(1)
-    //    val keyedStream: KeyedStream[SensorReading, Tuple] = dataStream.keyBy(0)
-    //    val dataStream = keyedStream.sum(2)
+    // 聚合，按照温度相加
+    // .keyBy(0)
+    // .sum(1)
+    // keyedStream要求范型为T,K；k为Tuple，当用_.id指定为String，范型K可以为String;否则需要按照Tuple定义
+    val keyedStream: KeyedStream[SensorReading, String] = dataStream.keyBy(_.id)
+    val dataSm = keyedStream.sum("temperature")
 
-    dataStream.print()
-//      .setParallelism(1)
+    dataSm.print()
+    //      .setParallelism(1)
 
 
     env.execute("my transform")
